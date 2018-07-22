c21d6f8bfe9bf15645f10360de2eb144794a23a2
John Huang <johnjunhh@gmail.com>
2018-07-22 17:57:13 -0400

    Added login view

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<!-- LOGIN PAGE -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login</title>
+		<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+	</head>
+	<body>
+		<div id="app">
+			{{ message }}
+		</div>
+		<script>
+			new Vue({
+				el: '#app',
+				data: {
+					message: 'Login!'
+				}
+			});
+		</script>
+	</body>
+</html>
